e6728dfbfa5876817334fcb14577bb2c3845a775
Murat Tasan <mmuurr@gmail.com>
2018-02-08 15:45:26 -0700

    better optionals handling

--- R/s3io_read.R
+++ R/s3io_read.R
@@ -5,9 +5,10 @@
 #' @param readfun [function] A read function where the first argument is a filepath.
 #'        The function's signature should look like \code{readfun(file, ...)}.
 #' @param ... Additional arguments forwarded on to \code{readfun}.
-#' @param localfile [string] The local filepath of the downloaded file.
-#' @param rm_localfile [boolean] Should the local filepath be deleted once it has been read?
+#' @param .localfile [string] The local filepath of the downloaded file.
+#' @param .rm_localfile [boolean] Should the local filepath be deleted once it has been read?
 #'        If \code{rm_localfile = TRUE}, then the read operation should read the entire file into memory.
+#' @param .get_object_args [list] Additional optional args passed on to \code{awscli::s3api_get_object}.
 #'
 #' @return The returned value from \code{readfun}.
 #' 
@@ -17,15 +18,14 @@
 #' }
 s3io_read <- function(bucket, key,
                       readfun, ...,
-                      localfile = tempfile(), rm_localfile = TRUE) {
-    tryCatch({
-        flog.debug("s3://%s/%s --> %s", bucket, key, localfile)
-        awscli::s3api_get_object(bucket, key, localfile)
-        flog.debug("reading from %s", localfile)
-        RV <- readfun(localfile, ...)
-    }, finally = {
-        if(rm_localfile) try_file_remove(localfile)
-    })
-    return(RV)
+                      .localfile = tempfile(),
+                      .rm_localfile = TRUE,
+                      .get_object_args = list()) {
+    if(.rm_localfile) on.exit(try_file_remove(.localfile), add = TRUE)
+    
+    flog.debug("s3://%s/%s --> %s", bucket, key, .localfile)
+    do.call(awscli::s3api_get_object, c(list(bucket, key, .localfile), .get_object_args))
+    flog.debug("reading from %s", .localfile)
+    readfun(.localfile, ...)
 }
 
--- R/s3io_write.R
+++ R/s3io_write.R
@@ -1,30 +1,34 @@
 #' @title Write a file to AWS S3
-#'
 #' @param obj The object to write.
 #' @param bucket [string] AWS S3 bucket name.
 #' @param key [string] AWS S3 object key.
 #' @param writefun [function] A write function where the first argument is the object to write and the second argument is a filepath.
 #'        The function's signature should look like \code{writefun(obj, file, ...)}.
 #' @param ... Additional arguments passed on to \code{writefun}.
-#' @param localfile [string] The local filepath for the initial write-to-disk.
-#' @param rm_localfile [boolean] Remove \code{localfile} once the copy-to-S3 is complete?
-#'
+#' @param .localfile [string] The local filepath for the initial write-to-disk.
+#' @param .rm_localfile [boolean] Remove \code{localfile} once the copy-to-S3 is complete?
+#' @param .put_object_args [list] Additional optional args passed on to \code{awscli::s3api_put_object}.
+#'        A common option you may want to specify, e.g., is content-type: \code{.put_object_args = list("content-type" = "application/json")}.
 #' @return The returned value from \code{writefun}.
-#'
 #' @examples
 #' \dontrun{
 #'   s3io_write(iris, "mybucket", "my/object/key.csv", readr::write_csv, col_names = TRUE)
 #' }
 s3io_write <- function(obj, bucket, key,
                        writefun, ...,
-                       localfile = tempfile(), rm_localfile = TRUE) {
-    tryCatch({
-        flog.debug("writing to %s", localfile)
-        RV <- writefun(obj, localfile, ...)
-        flog.debug("%s --> s3://%s/%s", localfile, bucket, key)
-        awscli::s3api_put_object(localfile, bucket, key)
-    }, finally = {
-        if(rm_localfile) try_file_remove(localfile)
-    })
-    return(RV)
+                       .localfile = tempfile(),
+                       .rm_localfile = TRUE,
+                       .put_object_args = list()) {
+    if(.rm_localfile) on.exit(try_file_remove(.localfile), add = TRUE)
+
+    flog.debug("writing to %s", .localfile)
+    retval <- withVisible(writefun(obj, .localfile, ...))
+    flog.debug("%s --> s3://%s/%s", .localfile, bucket, key)
+    do.call(awscli::s3api_put_object, c(list(.localfile, bucket, key), .put_object_args))
+
+    if(retval$visible) {
+        return(retval$value)
+    } else {
+        return(invisible(retval$value))
+    }
 }
